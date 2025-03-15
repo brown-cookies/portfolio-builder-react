101e1d3dfa43b6098cd9870544a7a3c363d6ef0c
brown-cookies <jesimielalmedejar1018@gmail.com>
2025-03-15 08:50:38 +0800

    misc: remove studio editor

--- src/routes/studio.tsx
+++ src/routes/studio.tsx
@@ -13,28 +13,11 @@ options={{
     '567bbc73d5524e959c7042af8f09de8fa0bd8a34221145e0b67c16639bd5add0',
   project: {
     type: 'web',
-    default: {
-      pages: [
-        { name: 'Home', component: '<h1>Home page</h1>' },
-        { name: 'About', component: '<h1>About page</h1>' },
-        { name: 'Contact', component: '<h1>Contact page</h1>' },
-      ],
-    },
   },
 }}
 /> */
 }
 
 function RouteComponent() {
-  return (
-    <StudioEditor
-      options={{
-        licenseKey:
-          '567bbc73d5524e959c7042af8f09de8fa0bd8a34221145e0b67c16639bd5add0',
-        project: {
-          type: 'web',
-        },
-      }}
-    />
-  )
+  return <h1>Hello, World</h1>
 }
